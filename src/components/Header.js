ae181c454e941c8358e1e55e8710fa23b5a50c00
Anant Gaire <56.anant@gmail.com>
2021-05-29 14:17:01 +0545

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return <Container>hi from header</Container>;
+};
+
+export default Header;
+
+const Container = styled.div`
+  height: 70px;
+  background-color: #090b13;
+`;
